6a1174461e92fdb71bb8a6e6866c57ef0ef411ba
Sandro <sandroodangelo@hotmail.com>
2026-02-01 00:41:37 +0100

    fix: correct base path case for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chill.it/',
+  base: '/Chill.it/',
   plugins: [
     vue(),
     vueDevTools(),
